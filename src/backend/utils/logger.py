f1fa49c1b8354cdbc7df628515eed79586c0a541
njoyb <soumobanerjee11@gmail.com>
2025-06-17 19:55:45 +0530

    added init files

---
+++ src/backend/utils/logger.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%SZ",
+    handlers=[logging.FileHandler("app.log"), logging.StreamHandler()],
+)
